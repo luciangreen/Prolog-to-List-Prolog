f5197bbab10dbb12ac9950c6cca87f74fc36ba19
Lucian Green <luciangreen@lucianacademy.com>
2022-05-27 00:23:11 +1000

    Update p2lpconverter.pl

--- p2lpconverter.pl
+++ p2lpconverter.pl
@@ -538,7 +538,8 @@ line(Word1) -->
 		"(",line(Word2),")",{Word1=[Word2]},!.
 line(Word1) -->
 		"(",line(Word2),"->",line(Word3),";",line(Word4),")",
-		{Word1=[[n,"->"],[Word2,Word3,Word4]]},!.
+		{(Word4=[[[[n,_]|_]|_]|_]->Word4=[Word41];Word4=Word41),
+		Word1=[[n,"->"],[Word2,Word3,Word41]]},!.
 line(Word1) -->
 		"(",line(Word2),"->",line(Word3),")",
 		{Word1=[[n,"->"],[Word2,Word3]]},!.
